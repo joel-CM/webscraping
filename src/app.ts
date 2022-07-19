d120f479a1854c8cd9cc6772fb9a28cbb8192888
joel-cM <joelchavezm19@gmail.com>
2022-07-19 12:44:37 -0300

    ...

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import cors from 'cors'
+// import cors from 'cors'
 import api from './services/api'
 
 const app = express()
@@ -7,14 +7,14 @@ const app = express()
 // settings
 const PORT = process.env.PORT ?? 3001
 app.set('port', PORT)
-const allowedOrigins = '*'
-const options: cors.CorsOptions = {
-  origin: allowedOrigins
-}
+// const allowedOrigins = '*'
+// const options: cors.CorsOptions = {
+//   origin: allowedOrigins
+// }
 
 // middlewares
 app.use(express.json())
-app.use(cors(options))
+// app.use(cors(options))
 
 // route
 app.get('/api', api)
