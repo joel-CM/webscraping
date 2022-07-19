221dd8f69a6f2bb12e9595bb2549bbcd36702a87
joel-cM <joelchavezm19@gmail.com>
2022-07-19 13:29:36 -0300

    config headers

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,13 @@ app.set('port', PORT)
 
 // middlewares
 app.use(express.json())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 
 // route
 app.get('/api', api)
